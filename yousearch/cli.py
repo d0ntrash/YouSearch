06c66ab62128ae4f56aae2afc33aadc5fac486a9
buecheko <buecheko@protonmail.com>
2020-02-01 21:50:40 +0100

    added navigation to browse large output

--- yousearch/cli.py
+++ yousearch/cli.py
@@ -1,12 +1,12 @@
 import curses
 from yousearch import youtube_api
+from yousearch import mpv_api
 
 
 class Cli():
     def __init__(self):
         self.screen = curses.initscr()
         self.rows, self.cols = self.screen.getmaxyx()
-
         curses.noecho()
 
     def show_start_screen(self):
@@ -28,13 +28,50 @@ class Cli():
         curses.noecho()
         return keystring.decode()
 
-    def show_search_results(self, result):
+    def get_direction(self, pad):
+        while True:
+            key = pad.getch()
+            if key == curses.KEY_UP:
+                return -1
+            if key == curses.KEY_DOWN:
+                return 1
+            if key == curses.KEY_ENTER or key == 10:
+                return 2
+
+    def show_search_results(self, results):
         self.screen.clear()
-        pad = curses.newpad(len(result)+2, self.cols)
+        pad = curses.newpad(len(results)+2, self.cols)
+        pad_rows, pad_cols = pad.getmaxyx()
         pad.addstr("Select a result to start the video!")
+        pad.keypad(True)
         line_pointer = 2
-        pad.refresh(line_pointer, 0, 0, 0, self.rows - 1, self.cols - 1)
-        curses.napms(2000)
+        cursor_pointer = 2
+        for result in results:
+            pad.addstr(line_pointer, 1,
+                       f"{line_pointer - 1}. {result['text']}")
+            line_pointer += 1
+
+        enter_flag = False
+        while not enter_flag:
+            pad.move(cursor_pointer, 0)
+            pad.chgat(curses.A_REVERSE)
+            if cursor_pointer >= self.rows:
+                pad.refresh(cursor_pointer - self.rows + 1, 0,
+                            0, 0, self.rows - 1, self.cols - 1)
+            else:
+                pad.refresh(0, 0, 0, 0, self.rows - 1, self.cols - 1)
+            key = self.get_direction(pad)
+            pad.chgat(curses.A_NORMAL)
+            if key == 2:
+                enter_flag = True
+            elif cursor_pointer + key in range(2, pad_rows):
+                cursor_pointer += key
+        return cursor_pointer - 2
+
+    def start_video_screen(self):
+        self.screen.clear()
+        self.screen.addstr("Starting video...")
+        self.screen.refresh()
 
     def cli(self):
         while True:
@@ -42,8 +79,11 @@ class Cli():
             video = youtube_api.Video(url)
             keystring = self.show_browse_screen()
             search_result = video.search_string(keystring)
-            if len(search_result) < 0:
-                self.show_search_results(search_result)
+            if len(search_result) == 0:
+                continue
+            result_index = self.show_search_results(search_result)
+            self.start_video_screen()
+            mpv_api.start_video(url, search_result[result_index]["start"])
 
     def test_pad(self):
         self.pad.addstr(5, 0, "Hallo")
