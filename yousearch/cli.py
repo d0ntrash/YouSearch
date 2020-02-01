9444fbf3feffe3e458c76588fb4a54fe04cd1b9b
buecheko <buecheko@protonmail.com>
2020-02-01 19:00:24 +0100

    added some functions

--- yousearch/cli.py
+++ yousearch/cli.py
@@ -1,38 +1,49 @@
 import curses
+from yousearch import youtube_api
 
 
 class Cli():
     def __init__(self):
         self.screen = curses.initscr()
         self.rows, self.cols = self.screen.getmaxyx()
-        self.pad = curses.newpad(self.rows, self.cols)
-        self.current_line = 0
 
         curses.noecho()
 
-    def create_new_pad(self, num_rows=None, num_cols=None):
-        self.pad = curses.newpad(
-            self.rows if num_rows is None else num_rows,
-            self.cols if num_cols is None else num_cols
-        )
-
     def show_start_screen(self):
-        self.screen.clean()
+        self.screen.clear()
         self.screen.addstr("Welcome to YouSearch!\n\n")
         self.screen.addstr("Enter a YouTube URL: ")
         self.screen.refresh()
         curses.echo()
         url = self.screen.getstr()
         curses.noecho()
-        return url
+        return url.decode()
 
     def show_browse_screen(self):
+        self.screen.clear()
         self.screen.addstr("Search for: ")
         self.screen.refresh()
         curses.echo()
-        s = self.screen.getstr()
+        keystring = self.screen.getstr()
         curses.noecho()
-        return s
+        return keystring.decode()
+
+    def show_search_results(self, result):
+        self.screen.clear()
+        pad = curses.newpad(len(result)+2, self.cols)
+        pad.addstr("Select a result to start the video!")
+        line_pointer = 2
+        pad.refresh(line_pointer, 0, 0, 0, self.rows - 1, self.cols - 1)
+        curses.napms(2000)
+
+    def cli(self):
+        while True:
+            url = self.show_start_screen()
+            video = youtube_api.Video(url)
+            keystring = self.show_browse_screen()
+            search_result = video.search_string(keystring)
+            if len(search_result) < 0:
+                self.show_search_results(search_result)
 
     def test_pad(self):
         self.pad.addstr(5, 0, "Hallo")
@@ -41,4 +52,4 @@ class Cli():
 
 
 cli = Cli()
-cli.show_browse_screen()
+cli.cli()
